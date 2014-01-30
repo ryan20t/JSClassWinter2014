f1d8bb0d831e89fb16b3f4535f05d0230adff5ae
ryan <ryan@10.240.209.150>
2014-01-29 20:14:56 -0500

    Merge origin/master

---
+++ public_html/week4/lab/script.js
@@ -0,0 +1,66 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+//USER INFORMATION COLLECTION
+//Declare JSON for user name/email
+var userdata = {
+    "name" : "",
+    "email" : ""
+};
+
+//Get elements and add Event Listeners to log data
+var fullname = document.getElementById('fullname');
+var emailAddress = document.getElementById('email');
+
+fullname.addEventListener('blur', collectName);
+emailAddress.addEventListener('blur', collectEmail);
+
+//Functions to log name and email information
+function collectName(){
+    var name = fullname.value;
+    userdata["name"] = name;
+}
+
+function collectEmail(){
+    var email = emailAddress.value;
+    userdata["email"] = email;
+}
+
+//MACHINE INFORMATION COLLECTION
+//Declare JSON for user's machine's information
+var machinedata = {
+    "winHeight" : "",
+    "winWidth" : "",
+    "userAgent" : "",
+    "docTitle" : ""
+};
+
+//Function declaration - collects machine info into JSON object
+function collectMachineInfo(){
+    machinedata["winHeight"] = window.innerHeight;
+    machinedata["winWidth"] = window.innerWidth;
+    machinedata["userAgent"] = window.navigator.userAgent;
+    machinedata["docTitle"] = document.title;
+    console.log(machinedata);
+    fullname.value = "";
+    emailAddress.value = "";
+}
+
+//Event listener to collect machine info on load
+window.addEventListener('load', collectMachineInfo);
+
+
+//RESULTS AND OUTPUT
+//Function to log results when results button is clicked
+function showResults() {
+    console.clear();
+    console.log(userdata);
+}
+
+var results = document.getElementById('showResults');
+
+results.addEventListener("click", showResults);
