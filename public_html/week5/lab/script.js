7b20b065f90084e98f423eb63f1a5c9c9153f03c
ryan <ryan@192.168.1.20>
2014-02-05 21:43:27 -0500

    Merge origin/master

--- public_html/week5/lab/script.js
+++ public_html/week5/lab/script.js
@@ -38,31 +38,62 @@ function submitForm() {
         var email = document.getElementById('email');
         var comments = document.getElementById('comments');
 
+
         if ( !fname.value.length ) {
                 console.log("Fname needs a length");
+                err_fname.innerHTML = "<strong>error: field is blank</strong>";
+                fname.className = "bad";
         } else if ( noSpaceAlphaValidate( fname.value ) === false ) {
                 console.log("Fname needs Alpha chars");
+                err_fname.innerHTML = "<strong>error: field is invalid</strong>";
+                fname.className = "bad";
         } else {
                 console.log("Fname is good");
+                err_fname.innerHTML = "";
+                fname.className = "good";
         }
         
         if ( !lname.value.length ) {
                 console.log("Lname needs a length");
+                err_lname.innerHTML = "<strong>error: field is blank</strong>";
+                lname.className = "bad";
         } else if ( spaceAlphaValidate( lname.value ) === false ) {
                 console.log("Lname needs Alpha chars");
+                err_lname.innerHTML = "<strong>error: field is invalid</strong>";
+                lname.className = "bad";
         } else {
                 console.log("Lname is good");
+                err_lname.innerHTML = "";
+                lname.className = "good";
         }
         
         if ( !email.value.length ) {
                 console.log("email needs a length");
+                err_email.innerHTML = "<strong>error: field is blank</strong>";
+                email.className = "bad";
         } else if ( emailValidate( email.value ) === false ) {
                 console.log("email is not valid");
+                err_email.innerHTML = "<strong>error: field is invalid</strong>";
+                email.className = "bad";
         } else {
                 console.log("email is good");
+                err_email.innerHTML = "";
+                email.className = "good";
         }
         
-        comments = strip_HTML(comments);
-        comments.value = strip_HTML(comments.value);
+        
+        if (comments.value.length > 150) {
+            commentsSpan.innerHTML = "<strong>comments must be less than 150 characters</strong>";
+            comments.className = "bad";
+        } else if (comments.value.length < 1) {
+            commentsSpan.innerHTML = "<strong>comments must not be blank</strong>";
+            comments.className = "bad";
+        } else if (comments.value.length < 150 && comments.value.length > 1) {
+            commentsSpan.innerHTML = "";
+            comments.className = "good";
+            comments.value = strip_HTML(comments.value);
+        }
+        
+        
 
 }
