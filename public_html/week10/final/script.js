f81b1e78ff6cbcb5c0fe24ece9faebb549abee76
ryan <ryan@10.240.208.82>
2014-03-12 19:18:50 -0400

    Merge origin/master

--- public_html/week10/final/script.js
+++ public_html/week10/final/script.js
@@ -2,12 +2,12 @@
 var docOneClicked = false;
 var docTwoClicked = false;
 var scrollToBottom = false;
-//var link1 = document.getElementById('agreeLink1');
-//var link2 = document.getElementById('agreeLink2');
+var divObject = document.getElementById('agreeScroll');
 
 //event listeners for document clicks
 document.getElementById('agreeLink1').addEventListener('click', onClick1);
 document.getElementById('agreeLink2').addEventListener('click', onClick2);
+divObject.addEventListener('scroll', onScroll);
 
 //function to indicate link 1 is clicked
 //and if link 2 is clicked and the div is scrolled, enable the radio buttons
@@ -30,5 +30,19 @@ function onClick2()
     if (scrollToBottom === true && docOneClicked === true)
     {
         document.getElementById('agree').disabled=false;
+    }
+}
+
+//function to check if div is fully scrolled and if both links are also
+//clicked, enable the radio buttons
+function onScroll()
+{
+    if (divObject.scrollTop === (divObject.scrollHeight - divObject.offsetHeight))
+    {
+        scrollToBottom = true;
+        if (docOneClicked === true && docTwoClicked === true)
+        {
+            document.getElementById('agree').disabled=false;
+        }
     }
 }
