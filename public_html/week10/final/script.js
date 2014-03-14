b30e687ef5ba4bb04d01b450cd03a41c4fa99c5a
ryan <ryan@192.168.1.3>
2014-03-14 17:57:57 -0400

    Merge origin/master

--- public_html/week10/final/script.js
+++ public_html/week10/final/script.js
@@ -2,26 +2,41 @@
 var docOneClicked = false;
 var docTwoClicked = false;
 var scrollToBottom = false;
+//variables for elements
 var divObject = document.getElementById('agreeScroll');
 var agree = document.getElementById('agree');
 var notagree = document.getElementById('notagree');
+var submitButton = document.getElementById('submitAgreement');
+//to determing if error messages are displayed
+var docMessageShown = false;
+var scrollMessageShown = false;
 
 //event listeners for document clicks
 document.getElementById('agreeLink1').addEventListener('click', onClick1);
 document.getElementById('agreeLink2').addEventListener('click', onClick2);
 divObject.addEventListener('scroll', onScroll);
-document.getElementById('submitAgreement').addEventListener('click', buttonClick);
+submitButton.addEventListener('click', buttonClick);
+
+//function to indicate you are OK to continue
+function radioSelected()
+{
+    submitButton.style.color="green";
+    document.getElementById('check').style.visibility="visible";
+    //console.log("hello");
+}
 
 //function to indicate link 1 is clicked
 //and if link 2 is clicked and the div is scrolled, enable the radio buttons
 function onClick1()
 {
     docOneClicked = true;
-    console.log(docOneClicked);
+    //console.log(docOneClicked);
     if (scrollToBottom === true && docTwoClicked === true)
     {
         agree.disabled=false;
         notagree.disabled=false;
+        agree.addEventListener('click', radioSelected);
+        notagree.addEventListener('click', radioSelected);
     }
 }
 
@@ -30,11 +45,13 @@ function onClick1()
 function onClick2()
 {
     docTwoClicked = true;
-    console.log(docTwoClicked);
+    //console.log(docTwoClicked);
     if (scrollToBottom === true && docOneClicked === true)
     {
         agree.disabled=false;
         notagree.disabled=false;
+        agree.addEventListener('click', radioSelected);
+        notagree.addEventListener('click', radioSelected);
     }
 }
 
@@ -45,10 +62,13 @@ function onScroll()
     if (divObject.scrollTop === (divObject.scrollHeight - divObject.offsetHeight))
     {
         scrollToBottom = true;
+        //console.log(scrollToBottom);
         if (docOneClicked === true && docTwoClicked === true)
         {
             agree.disabled=false;
             notagree.disabled=false;
+            agree.addEventListener('click', radioSelected);
+            notagree.addEventListener('click', radioSelected);
         }
     }
 }
@@ -56,33 +76,64 @@ function onScroll()
 //button click function
 function buttonClick()
 {
+    //documents read error?
+    if (!docOneClicked || !docTwoClicked)
+    {
+        if (docMessageShown === false) 
+        {
+            document.getElementById('docErrorSpan').style.visibility="visible";
+            docMessageShown = true;
+        }
+    }
+    
+    else { document.getElementById('docErrorSpan').style.visibility="hidden"; }
+    
+    //scroll to bottom error?
+    if (!scrollToBottom) 
+        {
+            if (scrollMessageShown === false) 
+            {
+                document.getElementById('scrollErrorSpan').style.visibility="visible";
+                scrollMessageShown = true;
+            }
+        }
+        
+    else { document.getElementById('scrollErrorSpan').style.visibility="hidden"; }
+        
+    
+    //if 3 conditions are satisfied
     if (docOneClicked && docTwoClicked && scrollToBottom)
     {
+        //if no agreement selection is made
         if (!agree.checked && !notagree.checked)
         {
             alert("You must choose to agree or not agree");
         }
         
+        //if agree
         else if (agree.checked)
         {
             document.getElementById('header1').style.visibility="hidden";
             document.getElementById('section1').style.visibility="hidden";
-            document.getElementById('section2').style.visibility="hidden";
-            document.getElementById('body').innerHTML = '<div class="welcome">Thank you for agreeing to our terms, and we hope you enjoy our service!</div>';
+            document.getElementById('docErrorSpan').style.visibility="hidden";
+            document.getElementById('section2').innerHTML = 'Thank you for agreeing to our terms, and we hope you enjoy our service!';
+            document.getElementById('section2').style.textAlign="center";
         }
         
+        //if disagree
         else if (notagree.checked)
         {
             alert("You have chosen to disagree with out terms. We're sorry to see you go.");
             document.getElementById('header1').style.visibility="hidden";
             document.getElementById('section1').style.visibility="hidden";
-            document.getElementById('section2').style.visibility="hidden";
+            document.getElementById('scrollErrorSpan').style.visibility="hidden";
+            document.getElementById('section2').innerHTML = 'Come back any time!';
+            document.getElementById('section2').style.textAlign="center";
         }
     }
     
-    else if (!docOneClicked || !docTwoClicked)
-    {
-        document.getElementById('section1').innerHTML += '<span class="error">You must read both documents to continue.</span>';
-        if (!scrollToBottom) {document.getElementById('scrollWrapper').innerHTML += '<span class="error">You must scroll to the bottom to continue.</span>';}
-    }
+    //console.log("doc1", docOneClicked);
+    //console.log("doc2", docTwoClicked);
+    //console.log("scroll", scrollToBottom);
+    
 }
